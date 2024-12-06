3fc5c2c63b994bdee2e5bb58e5b93dbcd9f1e851
[Dominik] <domidi3326@gmail.com>
2024-12-06 08:28:30 +0100

    deleted helper

--- Day 6/part1.py
+++ Day 6/part1.py
@@ -1,17 +1,15 @@
 #MSG: Automatisch generiert | Tag 6 | https://adventofcode.com/%7Byear%7D/day/%7Bday_number%7D
 
-import handler as h
-
-file_path = 'input.txt'
+file_path = r'Day6/input.txt'
 with open(file_path, "r") as file:
     file_content = file.read()
 
 lines_full = file_content.split("\n")
 
-file_path = 'example.txt'
+file_path = r'Day6/example.txt'
 with open(file_path, "r") as file:
     file_content = file.read()
 
 lines_example = file_content.split("\n")
 
-Code start hier - -
+print(lines_example)
